c125931cd263ac9b811edddbb9dc6017024e74fb
xieyehua <xieyehua@epmworld.net>
2017-11-23 11:50:49 +0800

    项目初始搭建

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  test() {
+     $('div').addClass('_div1');
+  }
+}
